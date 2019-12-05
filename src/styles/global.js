d744d80d6f5c10d3a82d247dfcbdc5105fe545da
patricfonseca <patric.ccomp@gmail.com>
2019-12-04 22:48:59 -0300

    Finalizada o módulo flux

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,5 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 import backgroundBorder from '../assets/images/backgroundBorder.svg';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default createGlobalStyle`
   @import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
